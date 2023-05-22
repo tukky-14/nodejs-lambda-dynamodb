5fe32bba786e483b40cdf2f7930bec6778cb86ec
tukky-14 <bigoonage0104@gmail.com>
2023-05-22 23:04:32 +0900

    add: logic to retrieve data from DynamoDB in Lambda

---
+++ get.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event) => {
+    // DynamoDBの設定
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const tableName = process.env.TABLE_NAME;
+
+    // データを取得するためのクエリパラメータ
+    const params = {
+        TableName: tableName,
+        Key: {
+            year: event.year,
+            month_and_day: event.month_and_day, // テーブルがパーティションキーとソートキーの組み合わせの場合、ソートキーも指定する
+        },
+    };
+
+    try {
+        const data = await dynamodb.get(params).promise();
+        const item = data.Item;
+        console.log('Data:', item);
+        return { statusCode: 200, body: JSON.stringify(item) };
+    } catch (error) {
+        console.error('Failed', error);
+        return { statusCode: 500, body: 'Error' };
+    }
+};
