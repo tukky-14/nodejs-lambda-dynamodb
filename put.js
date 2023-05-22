8a5b7362393c6b32f617c08b44e37452acb5b77b
tukky-14 <bigoonage0104@gmail.com>
2023-05-22 22:55:12 +0900

    add: logic to save data from Lambda to DynamoDB

---
+++ put.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event) => {
+    // DynamoDBの設定
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const tableName = process.env.TABLE_NAME;
+
+    // 登録するデータ
+    const item = {
+        year: event.year,
+        month_and_day: event.month_and_day,
+        diary: event.diary,
+    };
+
+    // DynamoDBにデータを登録
+    const params = {
+        TableName: tableName,
+        Item: item,
+    };
+
+    try {
+        await dynamodb.put(params).promise();
+        console.log('Success!!');
+        return { statusCode: 200, body: 'Success' };
+    } catch (error) {
+        console.error('Failed: ', error);
+        return { statusCode: 500, body: 'Error' };
+    }
+};
