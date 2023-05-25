7a0d15602f1c02e4796fb4556229c80d0ce47ff3
tukky-14 <bigoonage0104@gmail.com>
2023-05-25 20:10:35 +0900

    add: query.js

---
+++ query.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+    // DynamoDBの設定
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const tableName = process.env.TABLE_NAME;
+
+    // Queryパラメータの設定
+    const params = {
+        TableName: tableName,
+        KeyConditionExpression: 'pk = :pk',
+        ExpressionAttributeValues: {
+            ':pk': '2023',
+        },
+    };
+
+    try {
+        // Query実行
+        const data = await dynamodb.query(params).promise();
+        console.log('Query Result:', data.Items);
+        return data.Items;
+    } catch (err) {
+        console.log('Error:', err);
+        throw err;
+    }
+};
